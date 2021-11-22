be8e47911fa696788a13362f102be10aae153742
voronin.leonid <voronin.leonid@otr.ru>
2021-11-22 16:09:22 +0700

    Начальный коммит

---
+++ src/main/java/ru/otr/openshifttest/OpenshiftTestApplication.java
@@ -0,0 +1,13 @@
+package ru.otr.openshifttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/otr/openshifttest/OpenshiftTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otr.openshifttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenshiftTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
